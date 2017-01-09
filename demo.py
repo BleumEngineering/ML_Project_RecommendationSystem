64ba2710d30bb7092484b24042534d35e5b3ce6a
ForestDengBleum <forest.deng@bleum.com>
2017-01-09 08:25:37 +0800

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  5 16:32:11 2017
+
+@author: forest
+"""
+
+
+from crab.models.classes import MatrixPreferenceDataModel
+from crab.recommenders.knn.neighborhood_strategies import AllNeighborsStrategy
+from crab.recommenders.knn.neighborhood_strategies import NearestNeighborsStrategy
+
+# setup data
+movies = {'Marcel Caraciolo': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
+ 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5,
+ 'The Night Listener': 3.0},
+'Luciana Nunes': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5,
+ 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0,
+ 'You, Me and Dupree': 3.5},
+'Leopoldo Pires': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
+ 'Superman Returns': 3.5, 'The Night Listener': 4.0},
+'Lorena Abreu': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
+ 'The Night Listener': 4.5, 'Superman Returns': 4.0,
+ 'You, Me and Dupree': 2.5},
+'Steve Gates': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+ 'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0,
+ 'You, Me and Dupree': 2.0},
+'Sheldom': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+ 'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
+'Penny Frewman': {'Snakes on a Plane': 4.5, 'You, Me and Dupree': 1.0, 'Superman Returns': 4.0},
+'Maria Gabriela': {}}
+
+model = MatrixPreferenceDataModel(movies)
+strategy = AllNeighborsStrategy()
+strategy.user_neighborhood('Lorena Abreu', model)
+
+
